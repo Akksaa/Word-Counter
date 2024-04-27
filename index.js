568485271d0b99a72cf9a9b3c0a8679de086dc6a
Akksaa <shireensaeed786@gmail.com>
2024-04-27 05:35:39 +0500

    files added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 console.log(chalk.bgCyanBright(chalk.black(`\n WELCOME TO AQSA'S WORDS AND LETTERS COUNTER:\n`)));
